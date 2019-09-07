1d9876df648d104076006b222f2110315de65cdd
Simon Schumann <simon.schumann@web.de>
2019-09-07 12:34:34 +0200

    Added error message when trying to drop into undeployed alpine shell

--- alpine_kindle_kual/drop_into_alpine.sh
+++ alpine_kindle_kual/drop_into_alpine.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-/mnt/us/extensions/kterm/bin/kterm -e "sh /mnt/us/alpine.sh" -k 1 -o U -s 7
+if [ -f /mnt/us/alpine.sh ] && [ -f /etc/upstart/alpine.conf ] && [ -f /mnt/us/alpine.ext3 ] ; then
+	/mnt/us/extensions/kterm/bin/kterm -e "sh /mnt/us/alpine.sh" -k 1 -o U -s 7
+else
+	fbink -pmh -y -5 "Error: Required files missing. Deploy Alpine first!"
+fi
