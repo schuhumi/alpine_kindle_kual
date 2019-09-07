a22f558c789a3a0512b60a129d9e0adc409244ed
Simon Schumann <simon.schumann@web.de>
2019-09-07 13:05:46 +0200

    Script to display unzipping progress now stops automatically when unzipping stops

--- alpine_kindle_kual/unzip_progress.sh
+++ alpine_kindle_kual/unzip_progress.sh
@@ -4,9 +4,14 @@ BYTES_TOTAL="$(unzip -l /mnt/us/alpine.zip | tail -1 | cut -d' ' -f1)"
 KBYTES_TOTAL="$((BYTES_TOTAL/1024))"
 LIST_OF_FILES="$(unzip -l /mnt/us/alpine.zip | rev | cut -d' ' -f1 | rev | tail -n +4 | tac | tail -n +3)"
 PROGRESS="0"
-BYTES_EXTRACTED="0"
+KBYTES_EXTRACTED="0"
+KBYTES_EXTRACTED_OLD="1"
 while [ "$PROGRESS" -lt "99" ] ; do
 	KBYTES_EXTRACTED="$(cd /mnt/us && du -k -c alpine.ext3 alpine.sh alpine.conf | grep total | tail -1 | cut -d$'\t' -f1)"
+	if [ "$KBYTES_EXTRACTED" == "$KBYTES_EXTRACTED_OLD" ] ; then # unzipping process stopped
+		break
+	fi
+	KBYTES_EXTRACTED_OLD="$KBYTES_EXTRACTED"
 	PROGRESS="$(($KBYTES_EXTRACTED*100/KBYTES_TOTAL))"
 	echo "Extracted $KBYTES_EXTRACTED of $KBYTES_TOTAL kb.. ($PROGRESS%)"
 	sleep 10
